ea86b17fe8d22e7e1a94aec481067ecd1da7f94f
Herica Cadoni <71198776+IamHerica@users.noreply.github.com>
2021-04-25 19:26:41 -0300

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 void troca(int *xp, int *yp) // funcao chamada no selectionSort, para trocar o menor valor para a primeira posicao percorrida 
 {
     int temp = *xp;
@@ -20,20 +18,4 @@ void selectionSort(int *vetor, int cont)
          
     }
 }
-int main () {
 
-  int vetor[10];
-  int cont;
-
-  for (cont = 0; cont <10; cont++) {
-    scanf("%i", &vetor[cont]);
-  }
-
-  selectionSort (vetor, cont);
-
-  for (cont = 0; cont <10; cont++) {
-    printf("Valor na posicao [%d]: %d \n", cont, vetor[cont]);
-  }
-
-  return 0;
-}
