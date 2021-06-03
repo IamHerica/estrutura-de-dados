9514967bbb27e5e8706515b3d56e477006feb447
Herica Cadoni <71198776+IamHerica@users.noreply.github.com>
2021-06-02 22:28:13 -0300

    localizar arquivos

---
+++ localizar.cs
@@ -0,0 +1,38 @@
+//git clone https://github.com/MicrosoftDocs/mslearn-dotnet-files && cd mslearn-dotnet-files
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace files_module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = FindFiles("stores");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        static IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+    
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+    
+            foreach (var file in foundFiles)
+            {
+                if (file.EndsWith("sales.json"))
+                {
+                    salesFiles.Add(file);
+                }
+            }
+    
+            return salesFiles;
+        }
+    }
+}
